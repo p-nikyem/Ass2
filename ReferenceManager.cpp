413f4f0178c22a9df9813c269017d0b86d27ad14
p-nikyem <92038777+p-nikyem@users.noreply.github.com>
2022-02-24 20:50:22 -0500

    Add files via upload

---
+++ ReferenceManager.cpp
@@ -0,0 +1,28 @@
+#include "ReferenceManager.h"
+ReferenceManager::ReferenceManager() {
+
+};
+ReferenceManager::ReferenceManager(int capacity) {
+	ReferenceManager* storage;//= new ReferenceManager[capacity];
+};
+bool ReferenceManager::add(Reference& reference) {
+	if (size < capacity) {
+
+		storage[size] = reference;
+		size++;
+		return true;
+	}
+	else {
+		return false;
+		cout << "Limit exceeded";
+	}
+};
+int ReferenceManager::get(int pos) {
+
+};
+bool ReferenceManager::Delete(int pos) {
+
+};
+bool ReferenceManager::search(int id) {
+
+};
